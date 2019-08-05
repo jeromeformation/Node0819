5292d048d5ba6533b796c7bc98003b07a420e6d1
jerome <ambroise.jerome.formation@gmail.com>
2019-08-05 14:42:19 +0200

    Les evenements (match de basket)

---
+++ inc.js
@@ -0,0 +1,5 @@
+let number = 15;
+
+console.log(number++); // 15
+// 16
+console.log(++number); // 17
--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 // Récupération du module HTTP
-const http = require('http');
+const http = require("http");
 
 // Création du serveur web
 const server = http.createServer(
     (req, res) => {
-        res.end('<h1>Hola !</h1>');
+        res.end('<h1>Bonjour !</h1>');
     }
 );
 
 // Lancement du serveur
-server.listen(3000);
+server.listen(3000, () => {
+    console.log("Serveur web lancé sur http://localhost:3000");
+});
