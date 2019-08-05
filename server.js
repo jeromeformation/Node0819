47e98551c1326a14a256c025b1ade42bd524305f
jerome <ambroise.jerome.formation@gmail.com>
2019-08-05 16:40:03 +0200

    Routage simple

--- server.js
+++ server.js
@@ -2,13 +2,29 @@
 const http = require("http");
 
 // Création du serveur web
-const server = http.createServer(
-    (req, res) => {
-        res.end('<h1>Bonjour !</h1>');
-    }
-);
+const server = http.createServer((req, res) => {
+    // Récupération de l'url
+    const route = req.url;
 
-// Lancement du serveur
-server.listen(3000, () => {
+    // Système de routage
+    if (route === '/') {
+        res.write('<h1>Page d\'accueil</h1>');
+    } else if(route === '/contact') {
+        res.write('<h1>Page de contact</h1>');
+    } else {
+        // Définition du statut
+        res.statusCode = 404;
+
+        res.write('<h1>Page 404 : Not Found !</h1>');
+    }
+
+    // Envoi de la réponse au navigateur
+    res.end();
+});
+
+server.on('listening', () => {
     console.log("Serveur web lancé sur http://localhost:3000");
 });
+
+// Lancement du serveur
+server.listen(3000);
